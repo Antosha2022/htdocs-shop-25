5ceee716522f9a095a6a2c4089d2351fb6e6537d
Anton <marchenko.anton1980@gmail.com>
2022-05-29 22:08:15 +0300

    CreateUsersTable add 2 fields role_id and permission_id

--- crud/database/migrations/2014_10_12_000000_create_users_table.php
+++ crud/database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,8 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('permission_id');
         });
     }
 
