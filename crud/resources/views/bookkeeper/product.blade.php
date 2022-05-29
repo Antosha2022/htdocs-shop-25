0d7310f3fbbce89c5aa9b6b693abfd1bf804165f
Anton <marchenko.anton1980@gmail.com>
2022-05-29 04:08:36 +0300

    cheked for errors in form product.blade.php

--- crud/resources/views/bookkeeper/product.blade.php
+++ crud/resources/views/bookkeeper/product.blade.php
@@ -3,4 +3,19 @@
 @section('bookkeeper_content')
 <h3>карточка продукта</h3>
 
+    @if($errors->any())
+    <div class="alert alert-danger">
+      <ul>
+          @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+          @endforeach
+      </ul>
+    </div>
+    @endif
+
+
+<form>
+
+
+</form>
 @endsection
