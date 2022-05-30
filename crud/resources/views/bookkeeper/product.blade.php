71d9e7d3390d84e691b07a64e8af5555f04e3303
Anton <marchenko.anton1980@gmail.com>
2022-05-31 01:21:11 +0300

    change form control

--- crud/resources/views/bookkeeper/product.blade.php
+++ crud/resources/views/bookkeeper/product.blade.php
@@ -6,7 +6,7 @@
 
 <h3>карточка продукта</h3>
 
-    @if($errors->any())
+    <!-- @if($errors->any())
     <div class="alert alert-danger">
       <ul>
           @foreach ($errors->all() as $error)
@@ -14,12 +14,36 @@
           @endforeach
       </ul>
     </div>
-    @endif
+    @endif -->
 
 
-<form method="post" action="{{route('product-form')}}">
+<form method="post" action="product/submit">
   @csrf
 
+  <div class="form-group">
+    <label for="short_name">введіть скорочену назву</label>
+    <input type="text" name="short_name" placeholder="Азова" id="short_name" class="form-control">
+  </div>
+  <!-- <div class="form-group">
+    <label for="full_name">введіть повну назву</label>
+    <input type="text" name="name" placeholder="Азова" id="full_name" class="form-control">
+  </div> -->
+  <div class="form-group">
+    <label for="bar_code">введіть штрих-код</label>
+    <input type="text" name="bar_code" placeholder="Азова" id="bar_code" class="form-control">
+  </div>
+  <div class="form-group">
+    <label for="description">введіть інформацію для споживача</label>
+    <textarea class="form-control" name="description" id="description" rows="5"></textarea>
+  </div>
+  <div class="form-group">
+    <label for="retail_price">введіть ціну</label>
+    <input type="double" name="retail_price" placeholder="Азова" id="retail_price" class="form-control">
+  </div>
+
+
+
+<!--
   <div class="mb-1 row">
     <label for="short_name" class="col-sm-2 col-form-label">назва</label>
     <div class="col-sm-6">
@@ -43,8 +67,8 @@
     <div class="col-sm-6">
       <input type="double" class="form-control" id="retail_price">
     </div>
-  </div>
-<button type="submit" class="btn btn-warning">створити</button>
+  </div> -->
+<button type="submit" class="btn btn-warning mt-3">створити</button>
 
 </form>
 
