250039e97ed1c7af97d376ea828ef76867b86f82
Anton <marchenko.anton1980@gmail.com>
2022-05-31 05:30:06 +0300

    product-list styling

--- crud/resources/views/bookkeeper/productList.blade.php
+++ crud/resources/views/bookkeeper/productList.blade.php
@@ -4,12 +4,15 @@
 @parent
 <div class="container mt-3">
 <h3>продукти</h3>
-  <div>
+
       @foreach($data as $el)
-          <h5><b>{{ $el->subject }}<b></h5>
-          <p>{{$el->short_name}}</p>
-          <p><small>{{ $el->retail_price}}</small></p>
-          <a href="{{route('product-data-one',$el->id)}}"><button class= "btn btn-warning">детальніше</button></a>
+    <div>
+          <h5>{{$el->short_name}}</h5>
+          штрих-код: {{$el->bar_code}}</br>
+          <small>ціна: {{ $el->retail_price}}</small></br>
+          <a href="{{route('product-data-one',$el->id)}}"><button class= "btn btn-warning mt-2 ms-2">детальніше</button></a>
+          <hr class="text-warning border-2 opacity-50">
+    </div>
       @endforeach
-</div>
+
 @endsection
