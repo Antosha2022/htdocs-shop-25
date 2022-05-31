0bf6dc7eda78f47063f6422942e213cfa5fe1ec9
Anton <marchenko.anton1980@gmail.com>
2022-05-31 21:19:58 +0300

    bag PermissionServiceProvider RoleServiceProvider Class 'App\Providers\Blade' not found

--- crud/app/Providers/PermissionServiceProvider.php
+++ crud/app/Providers/PermissionServiceProvider.php
@@ -23,24 +23,6 @@ class PermissionServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      public function boot() {
-      Blade::directive('perm', function ($perm) {
-          return "<?php if(auth()->check() && auth()->user()->hasPermAnyWay({$perm})): ?>";
-      });
-      Blade::directive('endperm', function ($perm) {
-          return "<?php endif; ?>";
-      });
-      Blade::directive('allperms', function ($perms) {
-          return "<?php if(auth()->check() && auth()->user()->hasAllPerms({$perms})): ?>";
-      });
-      Blade::directive('endallperms', function ($perms) {
-          return "<?php endif; ?>";
-      });
-      Blade::directive('anyperms', function ($perms) {
-          return "<?php if(auth()->check() && auth()->user()->hasAnyPerms({$perms})): ?>";
-      });
-      Blade::directive('endanyperms', function ($perms) {
-          return "<?php endif; ?>";
-      });
+      
     }
 }
--- crud/app/Providers/RoleServiceProvider.php
+++ crud/app/Providers/RoleServiceProvider.php
@@ -23,11 +23,6 @@ class RoleServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      Blade::directive('role', function ($role) {
-          return "<?php if(auth()->check() && auth()->user()->hasRole({$role})): ?>";
-      });
-      Blade::directive('endrole', function ($role) {
-          return "<?php endif; ?>";
-      });
+    
     }
 }
