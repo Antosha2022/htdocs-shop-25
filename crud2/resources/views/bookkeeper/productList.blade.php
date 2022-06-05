a1eda1f0b09b89150b712789c445f957d1c7b91e
Anton <marchenko.anton1980@gmail.com>
2022-06-05 09:48:12 +0300

    style product list

--- crud2/resources/views/bookkeeper/productList.blade.php
+++ crud2/resources/views/bookkeeper/productList.blade.php
@@ -21,13 +21,21 @@
 
       @foreach($data as $el)
     <div>
+      <div class="row">
+        <div class="col-3">
+          <img height="150px" width="230px" src="/mediaFiles/img_product{{$el->id}}.png" alt="{{$el->short_name}}" />
+          </div>
+          <div class="col-6">
           <h5>{{$el->short_name}}</h5>
           штрих-код: {{$el->bar_code}}</br>
           <small>ціна: {{ $el->retail_price}} ГРН</br>
           останнє оновлення: {{ $el->updated_at}}</small></br>
-          <a href="{{route('product-data-one',$el->id)}}"><button class= "btn btn-outline-warning mt-2 ms-2">детальніше</button></a>
-          <hr class="text-warning border-2 opacity-50">
+          <a href="{{route('product-data-one',$el->id)}}"><button class= "btn btn-outline-secondary mt-2 ms-2 mb-2">детальніше</button></a>
+
     </div>
+    <hr class="text-info border-2 opacity-50">
+  </div>
+</div>
       @endforeach
 </div>
 @endsection
